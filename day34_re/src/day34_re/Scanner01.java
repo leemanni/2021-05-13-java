2ba7e7b318e47ab67e1d68856c58d0e0f2fc5e4a
leemanni <cjffydahs@gmail.com>
2021-05-13 19:43:28 +0900

    다시 한 번

---
+++ day34_re/src/day34_re/Scanner01.java
@@ -0,0 +1,16 @@
+package day34_re;
+
+import java.util.*;
+
+public class Scanner01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in) ;
+		
+		System.out.print("이름을 입력하세요 :");
+		String a =  sc.next();
+		
+		System.out.println("당신의 이름은 " + a+ "입니다.");
+	
+		
+	}
+}
